ad9e285020fce8978176277baf35a63fdf37939f
Anton-Christiansen <AntonChristiansen@proton.me>
2026-09-23 19:31:25 +0200

    Refactor(RepairService): Removed dead function "PrintCaseSummary".
    Closes #7

--- PeterPedal/Program.cs
+++ PeterPedal/Program.cs
@@ -238,12 +238,6 @@ class RepairService
         Console.WriteLine($"{result.CustomerInfo.FirstName} has paid {result.TotalPrice:F2} kr. The bike is ready to ride!");
     }
 
-    // Old summary print, replaced by the receipt in finishRepair(). No longer called anywhere.
-    public void PrintCaseSummary(string frameNumber) {
-	RepairCase c = FindCase(frameNumber);
-	Console.WriteLine("Case summary for " + c.FrameNumber + ": " + c.Problem);
-    }
-
     private RepairCase FindCase(string frameNumber)
     {
         foreach (var c in _cases)
